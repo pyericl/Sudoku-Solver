370e5244fae50609d1113778000216d981cfb275
pyericl <pyericl@gmail.com>
2022-02-10 13:36:22 -0600

    comment in App.js
    testing if can differentiate between branches

--- react_jsx_sudoku/src/App.js
+++ react_jsx_sudoku/src/App.js
@@ -8,6 +8,7 @@ function App() {
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
+      {/* this is test branch */}
       <SudokuGrid />
     </div>
   );
